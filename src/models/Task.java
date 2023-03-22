426857f66603c01fdb46591b1c82330f87901b37
Axeloooo <axelshz@gmail.com>
2023-03-22 13:22:59 -0600

    Project Setup and Models created

---
+++ src/models/Task.java
@@ -0,0 +1,34 @@
+package models;
+
+public class Task {
+    private final long ID;
+    private final String DESCRIPTION;
+    private final int DURATION;
+    private final int MAX_WINDOW;
+
+    public Task(long ID, String DESCRIPTION, int DURATION, int MAX_WINDOW) throws IllegalArgumentException {
+        if (ID == 0 || DESCRIPTION == null || DURATION == 0 || MAX_WINDOW == 0) {
+            throw new IllegalArgumentException("All fields must be filled out");
+        }
+        this.ID = ID;
+        this.DESCRIPTION = DESCRIPTION;
+        this.DURATION = DURATION;
+        this.MAX_WINDOW = MAX_WINDOW;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public String getDESCRIPTION() {
+        return DESCRIPTION;
+    }
+
+    public int getDURATION() {
+        return DURATION;
+    }
+
+    public int getMAX_WINDOW() {
+        return MAX_WINDOW;
+    }
+}
---
+++ src/models/Treatments.java
@@ -0,0 +1,28 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Treatments {
+    private final  ArrayList<Treatment> TREATMENTS;
+
+    public Treatments(ArrayList<Treatment> treatments) throws IllegalArgumentException {
+        if (treatments == null) {
+            throw new IllegalArgumentException("Treatments cannot be null");
+        }
+        this.TREATMENTS = treatments;
+    }
+
+    public ArrayList<Treatment> getTreatments() {
+        return TREATMENTS;
+    }
+
+    public Treatment getTreatment(int id) {
+        for (Treatment treatment : TREATMENTS) {
+            if (treatment.getId() == id) {
+                return treatment;
+            }
+        }
+        return null;
+    }
+
+}
