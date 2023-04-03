0b32908197c1eca2136828e3529df353b9dced2a
nicole-izdraila <113864594+nicole-izdraila@users.noreply.github.com>
2023-04-03 13:31:26 -0600

    Something

--- src/edu/ucalgary/oop/Main.java
+++ src/edu/ucalgary/oop/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     String url = "jdbc:mysql://localhost:3306/EWR";
     String username = "root";
-    String password = "AxOmSaPe15092002"; // Enter your personal password
+    String password = "Barcelona15"; // Enter your personal password
 
     ArrayList<Animal> animals = new ArrayList<>();
 
