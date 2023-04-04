f98e0eb149901ecb3606d53c8ea66f678956d894
nicole-izdraila <113864594+nicole-izdraila@users.noreply.github.com>
2023-04-04 15:40:13 -0600

    logic for treatment greater than 2 hours

--- src/edu/ucalgary/oop/Schedule.java
+++ src/edu/ucalgary/oop/Schedule.java
@@ -1,4 +1,5 @@
 package edu.ucalgary.oop;
+
 import java.util.*;
 
 public class Schedule {
@@ -15,8 +16,8 @@ public class Schedule {
         animalCount.put("fox", 0);
         animalCount.put("racoon", 0);
         animalCount.put("beaver", 0);
-   
-        for (int i = 0; i<24; i++) {
+
+        for (int i = 0; i < 24; i++) {
             schedule.put(i, new ToDo());
         }
     }
@@ -26,43 +27,50 @@ public class Schedule {
         for (Treatment treatment : treatments) {
             int treatmentStartHour = treatment.getStartHour();
             int treatmentMaxWindow = treatment.getTask().getMaxWindow();
-            if (treatmentMaxWindow == 1){
+            if (treatmentMaxWindow == 1) {
                 schedule.get(treatmentStartHour).addTask(treatment.getTask().getDescription());
-                int newTimeRemaining = schedule.get(treatmentStartHour).getTimeRemaining() - treatment.getTask().getDuration();
+                int newTimeRemaining = schedule.get(treatmentStartHour).getTimeRemaining()
+                        - treatment.getTask().getDuration();
                 schedule.get(treatmentStartHour).updateTimeRemaining(newTimeRemaining);
             }
-            if (treatmentMaxWindow == 2){
-                if (schedule.get(treatmentStartHour).getTimeRemaining() < treatment.getTask().getDuration()){          
+            if (treatmentMaxWindow == 2) {
+                if (schedule.get(treatmentStartHour).getTimeRemaining() < treatment.getTask().getDuration()) {
                     schedule.get(treatmentStartHour + 1).addTask(treatment.getTask().getDescription());
-                    int newTimeRemaining = schedule.get(treatmentStartHour + 1).getTimeRemaining() - treatment.getTask().getDuration();
+                    int newTimeRemaining = schedule.get(treatmentStartHour + 1).getTimeRemaining()
+                            - treatment.getTask().getDuration();
                     schedule.get(treatmentStartHour + 1).updateTimeRemaining(newTimeRemaining);
-                }
-                else {
+                } else {
                     schedule.get(treatmentStartHour).addTask(treatment.getTask().getDescription());
-                    int newTimeRemaining = schedule.get(treatmentStartHour).getTimeRemaining() - treatment.getTask().getDuration();
+                    int newTimeRemaining = schedule.get(treatmentStartHour).getTimeRemaining()
+                            - treatment.getTask().getDuration();
                     schedule.get(treatmentStartHour).updateTimeRemaining(newTimeRemaining);
+                }
+            } else {
+                if (treatmentMaxWindow > 2 && treatmentMaxWindow < 24) {
+                    int i = treatmentStartHour;
+                    while (i < treatmentMaxWindow && 1 - schedule.get(treatmentStartHour).getTimeRemaining()
+                            - treatment.getTask().getDuration() >= 0) {
+                        schedule.get(i).addTask(treatment.getTask().getDescription());
+                        int newTimeRemaining = schedule.get(i).getTimeRemaining() - treatment.getTask().getDuration();
+                        schedule.get(i).updateTimeRemaining(newTimeRemaining);
+                        i++;
+                    }
                 }
             }
         }
     }
-        
-
 
     public void addAnimals(ArrayList<Animal> animals) {
         for (Animal animal : animals) {
             if (animal.getSpecies().equals("porcupine")) {
                 this.animalCount.put("porcupine", animalCount.get("porcupine") + 1);
-            } 
-            else if (animal.getSpecies().equals("coyote")) {
+            } else if (animal.getSpecies().equals("coyote")) {
                 this.animalCount.put("coyote", animalCount.get("coyote") + 1);
-            }
-            else if (animal.getSpecies().equals("fox")) {
+            } else if (animal.getSpecies().equals("fox")) {
                 this.animalCount.put("fox", animalCount.get("fox") + 1);
-            }
-            else if (animal.getSpecies().equals("racoon")) {
+            } else if (animal.getSpecies().equals("racoon")) {
                 this.animalCount.put("racoon", animalCount.get("racoon") + 1);
-            }
-            else if (animal.getSpecies().equals("beaver")) {
+            } else if (animal.getSpecies().equals("beaver")) {
                 this.animalCount.put("beaver", animalCount.get("beaver") + 1);
             }
         }
@@ -80,23 +88,23 @@ public class Schedule {
         treatments.add(treatment);
     }
 
-    public ArrayList<Treatment> getTreatment(){
+    public ArrayList<Treatment> getTreatment() {
         return treatments;
     }
 
-    public ArrayList<FeedingSchedule> getFeeding(){
+    public ArrayList<FeedingSchedule> getFeeding() {
         return feedings;
     }
 
-    public ArrayList<CleaningCage> getCageCleaning(){
+    public ArrayList<CleaningCage> getCageCleaning() {
         return cageCleanings;
     }
 
-    public HashMap<String, Integer> getAnimalCount(){
+    public HashMap<String, Integer> getAnimalCount() {
         return animalCount;
     }
 
-    public HashMap<Integer, ToDo> getSchedule(){
+    public HashMap<Integer, ToDo> getSchedule() {
         return schedule;
     }
 }
