95089125b9fdd7b02b4eaca7df664db6854d138d
nicole-izdraila <113864594+nicole-izdraila@users.noreply.github.com>
2023-04-04 17:06:04 -0600

    documentation complete for Task class

--- src/edu/ucalgary/oop/Task.java
+++ src/edu/ucalgary/oop/Task.java
@@ -1,9 +1,8 @@
 package edu.ucalgary.oop;
 /**
  * The Task class is used for the Main Java file. It takes in
- * an animal Id, the Animal object, the Task object and the start hour for the
- * respective treatment being done per. animal.
- * 
+ * a task Id, the task description, the duration and the max window that the 
+ * tasks must be completed in.
  * @author Group 4
  * @since 2023-03-23
  */
@@ -12,7 +11,15 @@ public class Task {
   private final String DESCRIPTION;
   private final int DURATION;
   private final int MAXWINDOW;
-
+/**
+   * Constructor for the Task class
+   * 
+   * @param ID
+   * @param DESCRIPTION
+   * @param DURATION
+   * @param MAXWINDOW
+   * @throws IllegalArgumentException
+   */
   public Task(int ID, String DESCRIPTION, int DURATION, int MAXWINDOW) throws IllegalArgumentException {
     if (ID <= 0 || DESCRIPTION == null || DESCRIPTION.trim().isEmpty() || DURATION <= 0 || MAXWINDOW <= 0) {
       throw new IllegalArgumentException("Invalid input for Task");
@@ -23,23 +30,45 @@ public class Task {
     this.DURATION = DURATION;
     this.MAXWINDOW = MAXWINDOW;
   }
-
+/**
+   * returns the ID of the task
+   * 
+   * @return ID of task
+   */
   public int getID() {
     return ID;
   }
-
+/**
+   * returns the description of task
+   * 
+   * @return the description of task
+   */
   public String getDESCRIPTION() {
     return DESCRIPTION;
   }
-
+/**
+   * returns the duration of task
+   * 
+   * @return ID of animal
+   */
   public int getDURATION() {
     return DURATION;
   }
-
+/**
+   * returns the max window that the task must be completed
+   * 
+   * @return maxwindow of task
+   */
   public int getMAXWINDOW() {
     return MAXWINDOW;
   }
-
+/**
+   * returns a string of the task ID, task description,
+   * task duration, and max window
+   * 
+   * @return formatted String of what was listed above
+   *
+   */
   @Override
   public String toString() {
     return "ID: " + ID + ", Description: " + DESCRIPTION + ", Duration: " + DURATION + ", Max Window: " + MAXWINDOW;
