c56f6e5bc46be9bdafe9dffb6d14a56edff30e00
nicole-izdraila <113864594+nicole-izdraila@users.noreply.github.com>
2023-04-05 13:49:16 -0600

    more documentation

--- src/edu/ucalgary/oop/CleaningCage.java
+++ src/edu/ucalgary/oop/CleaningCage.java
@@ -1,9 +1,7 @@
 package edu.ucalgary.oop;
 /**
- * The Treatment class is used for this project. It takes in
- * an animal Id, the Animal object, the Task object and the start hour for the
- * respective treatment being done per. animal.
- * 
+ * The CageCleaning class is used for this project. It takes in  the 
+ * species, name, duration and whether the cage is cleaned
  * @author Group 4
  * @since 2023-03-23
  */
@@ -14,7 +12,12 @@ public class CleaningCage {
     private String name;
     private String description = "Clean cage";
     private int duration;
-
+/**
+   * Constructor for the CageCleaning class
+   * 
+   * @param species
+   * @param name
+   */
     public CleaningCage(String name, String species) {
         this.species = species;
         this.name = name;
@@ -24,23 +27,44 @@ public class CleaningCage {
         else
             this.duration = 5;
     }
-
+/**
+   * returns the duration it takes to clean the cage
+   * 
+   * @return duration of cage cleaning
+   */
     public int getDuration() {
         return duration;
     }
-
+/**
+   * returns the species of the animal
+   * 
+   * @return species of animal
+   */
     public String getSpecies() {
         return species;
     }
-
+/**
+   * returns the name of the animal
+   * 
+   * @return name of animal
+   */
     public String getName() {
         return name;
     }
-
+/**
+   * returns the description fro the animal
+   * 
+   * @return description of task
+   */
     public String getDescription() {
         return description + " for " + species;
     }
-
+/**
+   * returns a string of the task description, animal name, and duration to complete
+   * 
+   * @return formatted String of what was listed above
+   *
+   */
     @Override
     public String toString() {
         return "To " + description + " for: " + name + ", Duration: " + duration;
--- src/edu/ucalgary/oop/FeedingSchedule.java
+++ src/edu/ucalgary/oop/FeedingSchedule.java
@@ -1,5 +1,12 @@
 package edu.ucalgary.oop;
-
+/**
+ * The feedingSchedule class is used for this project. It takes in
+ * an animal species, name, whether they need to be fed, duration of task, 
+ * starthour, timewindow and prep
+ * 
+ * @author Group 4
+ * @since 2023-03-23
+ */
 public class FeedingSchedule {
     private String species;
     private String name;
@@ -8,7 +15,13 @@ public class FeedingSchedule {
     private int startHour;
     private int timeWindow;
     private int prep;
-
+/**
+   * Constructor for the FeedingSchedule class
+   * 
+   * @param name
+   * @param species
+   * @param type
+   */
     public FeedingSchedule(String name, String species, String type) {
         this.duration = 5;
         this.timeWindow = 3;
@@ -32,34 +45,69 @@ public class FeedingSchedule {
 
     }
 
+ /**
+   * returns the species of the animal
+   * 
+   * @return species of animal
+   */
     public String getSpecies() {
         return species;
     }
-
+/**
+   * returns the name of the animal
+   * 
+   * @return name of animal
+   */
     public String getName() {
         return name;
     }
-
+/**
+   * returns the description of the task
+   * 
+   * @return description of the task
+   */
     public String getDescription() {
         return description + " " + species;
     }
-
+/**
+   * returns the duration of the task
+   * 
+   * @return duration of the task
+   */
     public int getDuration() {
         return duration;
     }
-
+/**
+   * returns the start hour that the task can be performed
+   * 
+   * @return starthour of the task
+   */
     public int getStartHour() {
         return startHour;
     }
-
+/**
+   * returns the time window that the task can be performed
+   * 
+   * @return timeWindow of task
+   */
     public int getTimeWindow() {
         return timeWindow;
     }
-
+/**
+   * returns the prep time of the task
+   * 
+   * @return prep of task
+   */
     public int getPrep() {
         return prep;
     }
-
+/**
+   * returns a string of the task description, name, start hour
+   * time window, dration, and prep time
+   * 
+   * @return formatted String of what was listed above
+   *
+   */
     @Override
     public String toString() {
         return "To " + description + " " + name + ", Start time: " + startHour + ", Time Window: " + timeWindow
